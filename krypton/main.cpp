56a900a26a5282e7ec681ee0f336b4e7b46717e1
Jack <zyuchuan.sz@gmail.com>
2018-03-03 21:47:16 +0800

    Initial Commit

---
+++ krypton/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  krypton
+//
+//  Created by Jack Zou on 2018/3/3.
+//  Copyright © 2018年 jack.zou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
