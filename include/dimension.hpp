4aed8f52855e8af11f706c8fcea10a07dc3d4adb
zyuchuan <zyuchuan.sz@gmail.com>
2018-04-06 10:28:46 +0800

    sequence completed

--- include/dimension.hpp
+++ include/dimension.hpp
@@ -34,15 +34,24 @@ struct is_dimension<sequence<Args...>> : public std::integral_constant<bool, siz
  * Angle                | r      | radian        | R
  */
 
-// Base dimension
-// https://en.wikipedia.org/wiki/List_of_physical_quantities
-// Our world is built upon seven basic units
-// we add one more here to address angle
-// krypton defines eight basic units, in theroy, eight flags
-// is enough to address all units. However, considering
-// dimensions may share same sequence value, e.g. work and
-// moment both are ML^2T^-2, we add one more flag to handle
-// this situation.
+/******************************************************************
+* Base dimension
+* https://en.wikipedia.org/wiki/List_of_physical_quantities
+*
+* Our world is built upon seven basic units, theoretically seven
+* flags is enough to define all these seven units.
+* However, things are little complicated when we try to simulate
+* real word by programming:
+*
+*	1. we add one more flag to address angle and radian
+*	2. Some compound dimensions share common dimension formula
+		with others, like work and moment, both have same
+		dimension ML^2T^2, we need one more flag to distinguish
+		them.
+*
+* Hence in krypton, each dimension is defined by nine flags.
+*****************************************************************/
+
 using scalar        = sequence<_0, _0, _0, _0, _0, _0, _0, _0, _0>;
 using length        = sequence<_1, _0, _0, _0, _0, _0, _0, _0, _0>;
 using mass          = sequence<_0, _1, _0, _0, _0, _0, _0, _0, _0>;
@@ -53,17 +62,25 @@ using substance     = sequence<_0, _0, _0, _0, _0, _1, _0, _0, _0>;
 using intensity     = sequence<_0, _0, _0, _0, _0, _0, _1, _0, _0>;
 using angle         = sequence<_0, _0, _0, _0, _0, _0, _0, _1, _0>;
 
+using L = length;
+using T = time;
+using M = mass;
+
 // Compound dimensions
-using velocity                  = sequence<_1, _0, __1, _0, _0, _0, _0, _0>;   // LT^-1
-using acceleration              = sequence<_1, _0, __2, _0, _0, _0, _0, _0>;   // LT^-2
-using force                     = sequence<_1, _1, __2, _0, _0, _0, _0, _0>;   // MLT^-2
-using momentum                  = sequence<_1, _1, __1, _0, _0, _0, _0, _0>;   // MLT^-1
-using work                      = sequence<_2, _1, __2, _0, _0, _0, _0, _1>;   // ML^2T^-2
-using moment                    = sequence<_2, _1, __2, _0, _0, _0, _0, _0>;   // ML^2T^-2
-using angular_velocity          = sequence<_0, _0, __1, _0, _0, _0, _1, _0>;   // T^-1
-using angular_acceleration      = sequence<_0, _0, __2, _0, _0, _0, _1, _0>;   // T^-2
-using moment_of_inertia         = sequence<_2, _1,  _0, _0, _0, _0, _0, _0>;   // ML^2
-using density                   = sequence<__1,_1,  _0, _0, _0, _0, _0, _0>;   // ML^-1
+//using velocity                  = sequence<_1, _0, __1, _0, _0, _0, _0, _0>;   // LT^-1
+//using acceleration              = sequence<_1, _0, __2, _0, _0, _0, _0, _0>;   // LT^-2
+//using force                     = sequence<_1, _1, __2, _0, _0, _0, _0, _0>;   // MLT^-2
+//using momentum                  = sequence<_1, _1, __1, _0, _0, _0, _0, _0>;   // MLT^-1
+//using work                      = sequence<_2, _1, __2, _0, _0, _0, _0, _1>;   // ML^2T^-2
+//using moment                    = sequence<_2, _1, __2, _0, _0, _0, _0, _0>;   // ML^2T^-2
+//using angular_velocity          = sequence<_0, _0, __1, _0, _0, _0, _1, _0>;   // T^-1
+//using angular_acceleration      = sequence<_0, _0, __2, _0, _0, _0, _1, _0>;   // T^-2
+//using moment_of_inertia         = sequence<_2, _1,  _0, _0, _0, _0, _0, _0>;   // ML^2
+//using density                   = sequence<__1,_1,  _0, _0, _0, _0, _0, _0>;   // ML^-1
+
+using velocity = divide<L, T>::type;
+using acceleration = multiply<L, pow<T, -2>::type>::type;
+using force = multiply<multiply<M, L>::type, pow<T, -2>::type>::type;
 
 END_KR_NAMESPACE
 #endif /* dimension_h */
---
+++ include/dimension_static_check.h
@@ -0,0 +1,21 @@
+﻿//
+//  dimension_static_check.h
+//  krypto
+//
+//  Created by Jack Zou on 2018/3/4.
+//  Copyright © 2018年 jack.zou. All rights reserved.
+//
+
+#ifndef dimension_static_check_h
+#define dimension_static_check_h
+
+#include "sequence.hpp"
+#include "dimension.hpp"
+
+void dimension_static_check() {
+	static_assert(kr::equals<kr::velocity, kr::sequence<kr::_1, kr::_0, kr::__1, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0>>::value, "they are equal");
+	static_assert(kr::equals<kr::acceleration, kr::sequence<kr::_1, kr::_0, kr::__2, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0>>::value, "they are equal");
+	static_assert(kr::equals<kr::force, kr::sequence<kr::_1, kr::_1, kr::__2, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0, kr::_0>>::value, "they are equal");
+}
+
+#endif // dimension_static_check_h
